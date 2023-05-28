8c6f99688c366a1ea5273527695093c5fad4c6eb
Athos Damiani <athos.damiani@gmail.com>
2023-05-28 21:47:49 +0100

    build_trie helper function

--- R/trie.R
+++ R/trie.R
@@ -1,5 +1,9 @@
 #' Trie
 #' 
+#' Trie is used to store the lexicon in language model. We use it to limit
+#' the search space in decoder and quickly look up scores for a given token
+#' (completed word) or make prediction for incompleted ones based on smearing.
+#' 
 #' @export
 #' @rdname Trie
 Trie <- R6::R6Class(
@@ -19,6 +23,7 @@ Trie <- R6::R6Class(
       invisible(cpp_TrieWrapper_getRoot(self$ptr))
     },
     
+    #' @description Insert a token into trie with label
     #' @param indices a indices
     #' @param label a label
     #' @param score a score
@@ -30,6 +35,7 @@ Trie <- R6::R6Class(
       return(invisible(trie_node))
     },
     
+    #' @description Get the labels for a given token
     #' @param indices a indices
     #' @return a TrieNodePtr
     search = function(indices) {
@@ -38,20 +44,25 @@ Trie <- R6::R6Class(
       return(invisible(trie_node))
     },
     
-    
+    #' @description Smearing the trie using the valid labels inserted in the trie 
+    #' so as to get score on each node (incompleted token).
+    #' For example, if smear_mode is MAX, then for node "a" in path "c"->"a", we
+    #' will select the maximum score from all its children like "c"->"a"->"t",
+    #' "c"->"a"->"n", "c"->"a"->"r"->"e" and so on.
+    #' This process will be carry out recursively on all the nodes.
     #' @param smearMode a smearMode (see [SmearingModes] for options)
     #' @return invisible(NULL)
     smear = function(smearMode) {
       cpp_TrieWrapper_smear(self$ptr, smearMode)
-      invisible(self$ptr)
+      return(invisible(self$ptr))
     }
   ),
   
   active = list(
     
     #' @field rootIdx the rootIdx.
-    #' @field maxChildren The maximum number of childern for each node. It is
-    #' usually the size of letters or phonmes.
+    #' @field maxChildren The maximum number of children for each node. It is
+    #' usually the size of letters or phonemes.
     #' @field ptr set and get the pointer to a Trie instance.
     ptr = function(new_ptr) {
       if(!missing(new_ptr)) private$ptr_ <- new_ptr
@@ -84,12 +95,14 @@ Trie <- R6::R6Class(
 
 #' TrieNode
 #' 
+#' TrieNode is the trie node structure in Trie.
+#' 
 #' @export
 #' @rdname TrieNode
 TrieNode <- R6::R6Class(
   "TrieNode",
   public = list(
-    #' @param idx a idx
+    #' @param idx an integer. Node index
     #' @return TrieNode
     initialize = function(idx = NULL) {
       private$idx_ <- idx
@@ -121,3 +134,38 @@ TrieNode <- R6::R6Class(
     idx_ = NULL
   )
 )
+
+
+#' Trie Builder
+#'
+#' Helper to build a trie with a Dictionary.
+#'
+#' @param lm a KenLM instance
+#' @param lexicon a named list with the words and its tokens.
+#' @param word_dict a Dictionary instance with the words. 
+#' @param token_dict a DIctionary instance with the tokens.
+#' @param separator_idx an integer. The index of the separator token.
+#'
+#' @returns a populated Trie instance.
+#' @export
+#'
+#' @examples
+build_trie <- function(lm, token_dict, lexicon, word_dict, separator_idx) {
+  trie <- Trie$new(token_dict$index_size(), separator_idx)
+  start_state <- lm$start(FALSE)
+  lexicon <- list2env(lexicon, hash = TRUE)
+  for(word in names(lexicon)) {
+    spellings <- lexicon[[word]]
+    usr_idx <- word_dict$get_index(word)
+    score <- lm$score(start_state, usr_idx)[[2]]
+    for(spelling in spellings) {
+      token_dict$map_entries_to_indices(spelling) %>%
+        pack_replabels(token_dict, 1) %>%
+        trie$insert(usr_idx, score)
+    }
+  }
+  trie$smear(SmearingModes$MAX)
+  return(trie)
+}
+
+
